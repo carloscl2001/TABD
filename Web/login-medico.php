751023bb626622518c3eae85bc171df4187a2773
robeerivero <roberto.riverodiaz@alum.uca.es>
2024-04-23 17:16:48 +0200

    Update login-medico.php

--- Web/login-medico.php
+++ Web/login-medico.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <!-- Enlaces a los archivos CSS -->
     <link rel="stylesheet" href="css/register.css">
-    <!-- Enlace al archivo JavaScript -->
+    <!-- Enlace al archivo JavaScript hola jefe -->
     
 </head>
 <body>
